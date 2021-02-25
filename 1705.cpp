5cc780e46077e4269a7b0482312c87ebe11a5c66
ChunYu-Wei <root@DESKTOP-71O1M1A.localdomain>
2021-02-25 21:05:56 +0800

    1721 modify

--- 1705.cpp
+++ 1705.cpp
@@ -21,66 +21,7 @@ int main(){
 	   cin >> tem2 ; 
 	   days.push_back(tem2);
 	}
-	
-	vector<int> status;
-	status.resize(40000);
-
-	for(int i = 0;i < status.size();i++){
-		status[i] = 0;
-	}
-	int dayth = 0;
-	int max_day = 0;
-	int count = 0;
-	do{
 
 
-		if(dayth < days.size() && days[dayth]){
-			status[days[dayth]-1] += apples[dayth];
-			if(days[dayth]-1 > max_day) max_day = days[dayth]-1;
-		}	
-
-		//search apple
-	
-	/*	
-		bool find = 0;
-
-		for(int i = 0;i <= max_day;i++){
-			if(status[i] != 0){
-				find = 1;
-				status[i] -= 1;
-				break;
-			}
-		}
-
-		if(find) count++;
-	*/
-
-		/*
-		cout << "---------" << endl;
-		for(int i = 0;i <= max_day;i++) cout << status[i];
-		cout << endl;
-		cout << "count: " << count << "   dayth: " << dayth << endl;
-		 */
-		
-
-		if(max_day > 0){
-			int idx = 0;
-			while(idx <= max_day && status[idx] == 0) idx++;
-			count += (status[idx] > max_day+1)? max_day+1: status[idx] ;
-			for(int i = 0;i <= max_day;i++){
-				status.erase(status.begin());
-			}
-			max_day -= idx;
-		}
-		else if(max_day == 0){
-			count += (status[0] == 0)? 0:1;
-			max_day--;
-		}	
-		//update
-		dayth++;
-	}
-	while(max_day >= 0 || dayth < days.size());
-
-	cout << count << endl;
 	return 0;
 }
--- 1721.cpp
+++ 1721.cpp
@@ -1,10 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
 struct ListNode{
 	int val;
 	ListNode *next;
-	ListNode() : val(0), next(nullptr){}
-	ListNode(int x) : val(x), next(nullptr){}
+	ListNode() : val(0), next(NULL){}
+	ListNode(int x) : val(x), next(NULL){}
 	ListNode(int x, ListNode *next) : val(x), next(next) {}
-}
+};
 
 class Solution {
 public:
@@ -12,7 +16,7 @@ public:
 
 		int count = 1;
 		ListNode* a = head;
-		while(a->next != nullptr){
+		while(a->next != NULL){
 			a = a->next;
 			count++;	
 		}
@@ -30,9 +34,52 @@ public:
 		b->val = a->val;
 		a->val = count;
 	}
-}
+
+	void printNodes(ListNode* head){
+		if(head != NULL) {
+			cout << head->val << " " ;
+			printNodes(head->next);
+		}
+	}
+};
 
 int main(void){
-	ListNode* tem1 = 
+	ListNode tem;
+	ListNode tem2;
+	ListNode tem3;
+	ListNode tem4;
+	ListNode tem5;
+
+	ListNode* ptem;
+	ListNode* ptem2;
+	ListNode* ptem3;
+	ListNode* ptem4;
+	ListNode* ptem5;
+
+	tem.val = 1;
+	tem2.val = 2;
+	tem3.val = 3;
+	tem4.val = 4;
+	tem5.val = 5;
+
+	ptem = &tem;
+	ptem2 = &tem2;
+	ptem3 = &tem3;
+	ptem4 = &tem4;
+	ptem5 = &tem5;
+
+	ptem->next = ptem2;
+	ptem2->next = ptem3;
+	ptem3->next = ptem4;
+	ptem4->next = ptem5;
+	
+	Solution s;
+
+	s.printNodes(ptem);
+	cout << endl;
+	s.swapNodes(ptem,2);
+	s.printNodes(ptem);
+
+
 
 }
