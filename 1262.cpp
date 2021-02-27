cd54da090883b7478a19d1a503a581d1ba49b86b
ChunYu-Wei <root@DESKTOP-71O1M1A.localdomain>
2021-02-27 23:11:53 +0800

    1262

--- 1262.cpp
+++ 1262.cpp
@@ -29,25 +29,36 @@ int main(){
 
 	int sum  = 0;
 	for(int i = 0;i < 3;i++){
-		sort(list[i].begin(),list[i].end(), greater<int>());
 		if(i == 0) sum += accumulate(list[i].begin(),list[i].end(),0);
-		else if(i == 1){
-			int nu = list[i].size() - list[i].size()%3;
-			for(int j = 0;j < nu;j++) sum += list[i][j];	
-		} 
-		else if(i == 2){
-			int nu = list[i].size() - list[i].size()%3;
-			for(int j = 0;j < nu;j++) sum += list[i][j];	
-		} 
+		else sort(list[i].begin(),list[i].end(), greater<int>());
 	}
 
-	int head1 = list[1].size() - list[1].size()%3;
-	int head2 = list[2].size() - list[2].size()%3;
-	int left1 = list[1].size()%3;
-	int left2 = list[2].size()%3;
-	int left = (left1>left2) ? left2 : left1 ;	
-	for(int i = 0;i < left;i++){
-		sum += list[1][head1+i] + list[2][head2+i];
+	while(1){
+	   int c1 = 0,c2 = 0,c3 = 0;
+		if(list[1].size() >= 3) c1 = list[1][0] + list[1][1] + list[1][2];
+		if(list[2].size() >= 3) c2 = list[2][0] + list[2][1] + list[2][2];
+		if(list[1].size() >= 1 && list[2].size() >= 1) c3 = list[1][0] + list[2][0];
+		int maxc = max(c1,max(c2,c3));
+		if(maxc == 0) break;
+		if(maxc == c3){
+			sum += c3;
+			list[1].erase(list[1].begin());
+			list[2].erase(list[2].begin());
+		}
+		else if(maxc == c1){
+			sum += c1;
+			list[1].erase(list[1].begin());
+			list[1].erase(list[1].begin());
+			list[1].erase(list[1].begin());
+		}
+		else{
+			sum += c2;
+			list[2].erase(list[2].begin());
+			list[2].erase(list[2].begin());
+			list[2].erase(list[2].begin());	
+		}
+			
+	
 	}
 
 	cout << sum << endl;
